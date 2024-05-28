710a7db692a88462c53f3183cb31df84f993fd71
dhanasanjay <130682656+dhanasanjay@users.noreply.github.com>
2024-05-28 18:27:41 +0530

    calculator

---
+++ calculator/calci.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById("display").innerText = "";
+}
+
+function appendToDisplay(value) {
+    document.getElementById("display").innerText += value;
+}
+
+function calculate() {
+    const display = document.getElementById("display");
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = "Error";
+    }
+}
